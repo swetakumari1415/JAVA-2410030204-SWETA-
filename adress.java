eeb215979d5e614efb251e92c63b4056ee89d545
swetakumari1415 <swetakumari8541961415@gmail.com>
2025-08-23 11:07:39 +0530

    Create java.adress

---
+++ adress.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class adress{
+    public static void main(String[] args){
+        System.out.println("vatika hostel/cc15,block C, Uttar Pradesh 201310");
+        System.out.print("VATIKA HOSTEL Greater Noida, ");
+        System.out.print("Uttar Pradesh 201310");
+    }
+}
